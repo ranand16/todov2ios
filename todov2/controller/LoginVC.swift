0a995064ad7b11acd06334a3dee3aae9dca13372
ranand16 <ranand16@gmail.com>
2018-06-07 06:47:49 +0530

    Login successfully .!

--- todov2/controller/LoginVC.swift
+++ todov2/controller/LoginVC.swift
@@ -5,12 +5,13 @@
 //  Created by Rishabh Anand on 03/06/18.
 //  Copyright © 2018 Rishabh Anand. All rights reserved.
 //
+
 import UIKit
 import Alamofire
 import SwiftyJSON
 
 class LoginVC: UIViewController {
-
+    
     var parameters: Parameters = ["username":"ranand", "password":"ris"] ;
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,13 @@ class LoginVC: UIViewController {
     
     @IBAction func onLoginPressed(_ sender: Any) {
         Alamofire.request("http://localhost:3000/api/login", method: .post, parameters: parameters, encoding: URLEncoding.httpBody).responseJSON { response in
-            print(response.result.value!)
-            print(response.result.value as! loginResponseStruct)
+            let json = JSON(response.result.value!)
+            print(json)
+            if(json["code"].exists()){
+                print("wrong!!")
+            }else{
+                self.performSegue(withIdentifier: "toDashboard", sender: self)
+            }
             
         }
     }
